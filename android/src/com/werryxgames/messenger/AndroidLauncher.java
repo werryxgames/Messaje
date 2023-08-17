41cf078f8a13fbc666c435c6583db700d08f7439
werryxgames <103959882+werryxgames@users.noreply.github.com>
2023-08-17 18:16:39 +0300

    Initial commit

---
+++ android/src/com/werryxgames/messenger/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.werryxgames.messenger;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.werryxgames.messenger.MessengerJava;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MessengerJava(), config);
+	}
+}
